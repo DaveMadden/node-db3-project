0cb1933bc4392df5961d5f26dd5257cae66ca551
Dave Madden <davelearnsstuff@gmail.com>
2021-12-08 13:07:57 -0800

    findsteps complete

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -12,7 +12,6 @@ async function find() { // EXERCISE A
 }
 
 async function findById(scheme_id) { // EXERCISE B
-  console.log(scheme_id)
   const rows = await db('schemes as sc')
     .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
     .select('sc.scheme_name', "st.*")
@@ -38,36 +37,27 @@ async function findById(scheme_id) { // EXERCISE B
   return result;
 }
 
-function findSteps(scheme_id) { // EXERCISE C
-  /*
-    1C- Build a query in Knex that returns the following data.
-    The steps should be sorted by step_number, and the array
-    should be empty if there are no steps for the scheme:
+async function findSteps(scheme_id) { // EXERCISE C
+  const rows = await db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('sc.scheme_name', "st.step_id", "st.step_number", "st.instructions")
+    .where('sc.scheme_id', scheme_id)
+    .orderBy('st.step_number')
 
-      [
-        {
-          "step_id": 5,
-          "step_number": 1,
-          "instructions": "collect all the sheep in Scotland",
-          "scheme_name": "Get Rich Quick"
-        },
-        {
-          "step_id": 4,
-          "step_number": 2,
-          "instructions": "profit",
-          "scheme_name": "Get Rich Quick"
-        }
-      ]
-  */
+    if (rows[0].step_id === null){
+      return [];
+    }
+
+  return rows;
 }
 
-function add(scheme) { // EXERCISE D
+async function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+async function addStep(scheme_id, step) { // EXERCISE E
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
